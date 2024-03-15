d5c1055227fb37f0b53bf73c199640d1858876ee
Robin Khuc <robin.khuc@beyondedge.co>
2024-03-15 22:36:12 +0700

    Add meal detail page

--- app/meals/[meal-id]/page.js
+++ app/meals/[meal-id]/page.js
@@ -1,10 +1,39 @@
-const MealDetails = () => {
+import { notFound } from 'next/navigation';
+import Image from 'next/image';
+
+// import styles and assets
+import styles from './page.module.css';
+import { getMeal } from '@/lib/meals';
+
+export default function MealDetails({ params }) {
+  const meal = getMeal(params['meal-id']);
+  if (!meal) {
+    notFound();
+  }
+  meal.instructions = meal.instructions.replace(/\n/g, '<br />');
+
   return (
     <>
-      <header></header>
-      <main></main>
+      <header className={styles.header}>
+        <div className={styles.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={styles.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={styles.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={styles.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={styles.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions,
+          }}
+        ></p>
+      </main>
     </>
   );
-};
-
-export default MealDetails;
+}
